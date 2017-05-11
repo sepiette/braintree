6f55fec5ed0d7375949a6f12d44e0ea4b8286343
Sara Piette <sepiette@gmail.com>
2017-05-11 16:29:02 -0400

    successful transaction path via braintree payment gateway with credit card

--- app/assets/javascripts/payment.js
+++ app/assets/javascripts/payment.js
@@ -7,43 +7,62 @@ var requestClientToken = function () {
   })
 }
 
-var makePaymentRequest = function() {
+var createClientGateway = function() {
   
   requestClientToken().done(function(response) {
     
     braintree.client.create({
       authorization: response.token
     }, function(clientErr, clientInstance) {
-        if(clientErr){
-          console.log(clientErr);
-          return;
-        }
-        console.log(clientInstance);
-        var data = {
-          creditCard: {
-            number: $('#cc-number')[0].value,
-            cvv: $('#cvv')[0].value,
-            expirationDate: $('#expiration-date')[0].value,
-            options: {
-              validate: false
-            }
-          }
-        };
+        if(clientErr){ throw new Error(clientErr); }
 
-        clientInstance.request({
-          endpoint: 'payment_methods/credit_cards',
-          method: 'post',
-          data: data
-        }, function (requestErr, response) {
-          if (requestErr) { throw new Error(requestErr); }
-          console.log('Got nonce:', response.creditCards[0].nonce);
-        });
+        console.log(clientInstance);
+        makePaymentRequest(clientInstance);
+
       });
   });
 }
 
+var makePaymentRequest = function(clientInstance) {
+  var data = {
+    creditCard: {
+      number: $('#cc-number')[0].value,
+      cvv: $('#cvv')[0].value,
+      expirationDate: $('#expiration-date')[0].value,
+      options: {
+        validate: false
+      }
+    }
+  };
+  clientInstance.request({
+      endpoint: 'payment_methods/credit_cards',
+      method: 'post',
+      data: data
+    }, function (requestErr, response) {
+        if (requestErr) { throw new Error(requestErr); }
 
+        console.log('Got nonce:', response.creditCards[0].nonce);
+        completeTransaction(response.creditCards[0].nonce);
+  }); 
+}
+var completeTransaction = function(nonce) {
+   $.ajax({
+      url: '/checkout',
+      method: 'POST',
+      data: {
+        payment_method_nonce: nonce
+      },
+      dataType: 'json'
+    })
+   .done(function(response) {
+    console.log(response);
+    $('body').append('<h2> Thank you. '+response.transaction_status+' </h2>');
+   })
+   .fail(function(err) {
+    console.log(err);
+   });
+}
 
 $(document).ready(function(){
-  $('#checkout-form').on('submit', makePaymentRequest);
+  $('#checkout-form').on('submit', createClientGateway);
 });
--- app/controllers/payment_controller.rb
+++ app/controllers/payment_controller.rb
@@ -1,11 +1,29 @@
 class PaymentController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def show
 
   end
 
   def checkout
     nonce_from_client = params[:payment_method_nonce]
+    result = Braintree::Transaction.sale(
+          :amount => "10.00",
+          :payment_method_nonce => 'fake-valid-nonce',
+          :options => {
+            :submit_for_settlement => true
+          }
+        )
+    if(result.success?)
+      @transaction = result.transaction
+      render :json => {'transaction_status': @transaction.status, 'transaction_type': @transaction.payment_instrument_type}
+    else
+      @errors = result.errors
+      render :json => {'errors': @errors}
+      format.html { render :action => "show" }
+    end
+
+    
   end
 
   def generate_token
